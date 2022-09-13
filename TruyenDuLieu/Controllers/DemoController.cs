9a71a28531a8aadcb23bea499aacce3f9072d38a
Hien Luong <hyhien@gmail.com>
2022-09-13 07:57:59 +0700

    Demo ViewBag, ViewData

---
+++ TruyenDuLieu/Controllers/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TruyenDuLieu.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.TenMon = "Công nghệ NET";
+            return View();
+        }
+
+        public IActionResult OtherIndex()
+        {
+            ViewData["TenMon"] = ".NET Technology";
+            return View("Index");
+        }
+    }
+}
