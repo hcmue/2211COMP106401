2ca48ef8010326f838fd2e418a94e7df48121269
Hien Luong <hyhien@gmail.com>
2022-09-13 09:06:28 +0700

    Hanlde calculate

--- TruyenDuLieu/Controllers/CalculatorController.cs
+++ TruyenDuLieu/Controllers/CalculatorController.cs
@@ -8,5 +8,22 @@ namespace TruyenDuLieu.Controllers
         {
             return View();
         }
+
+        public IActionResult Calculate(double SoHang01, double SoHang02, string PhepToan)
+        {
+            //var ketqua = $"{SoHang01} {PhepToan} {SoHang02} = ";
+            //return Content(ketqua);
+            ViewBag.SoHang01 = SoHang01;
+            ViewBag.SoHang02 = SoHang02;
+            ViewBag.PhepToan = PhepToan;
+            double kq = 0;
+            switch (PhepToan)
+            {
+                case "*": kq = SoHang01 * SoHang02; break;
+                case "^": kq = Math.Pow(SoHang01, SoHang02); break;
+            }
+            ViewBag.KetQua = kq;
+            return View("Index");
+        }
     }
 }
