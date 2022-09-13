6b4b2074e2abc099bd1c8edcca43ecb9c2b8ff46
Hien Luong <hyhien@gmail.com>
2022-09-13 08:28:16 +0700

    Calculator GUI

---
+++ TruyenDuLieu/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TruyenDuLieu.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
