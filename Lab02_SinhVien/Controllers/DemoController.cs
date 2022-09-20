44134a32801497b3312c4903e9945e43baf66c6d
Hien Luong <hyhien@gmail.com>
2022-09-20 10:05:02 +0700

    Demo upload file

---
+++ Lab02_SinhVien/Controllers/DemoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab02_SinhVien.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile SingleFile)
+        {
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "HinhAnh", SingleFile.FileName);
+            using (var myfile = new FileStream(fullPath, FileMode.Create))
+            {
+                SingleFile.CopyToAsync(myfile);
+            }
+            return View("Upload");
+        }
+
+
+        [HttpPost]
+        public IActionResult UploadFiles(List<IFormFile> MultiFile)
+        {
+            foreach (var SingleFile in MultiFile)
+            {
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "HinhAnh", SingleFile.FileName);
+                using (var myfile = new FileStream(fullPath, FileMode.Create))
+                {
+                    SingleFile.CopyToAsync(myfile);
+                }
+            }
+            return View("Upload");
+        }
+    }
+}
