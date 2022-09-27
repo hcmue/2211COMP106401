3344ab0cd6db6d82d6dc9df81bf171f8d5d6b472
Hien Luong <hyhien@gmail.com>
2022-09-27 07:42:20 +0700

    Demo Sync

---
+++ Demo/Demo/Controllers/Demo.cs
@@ -0,0 +1,24 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Demo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Sync()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            DemoSyncAsync.A();
+            DemoSyncAsync.B();
+            DemoSyncAsync.C();
+            stopWatch.Stop();
+
+            return Json(new { 
+                Type = "Sync",
+                Total = stopWatch.ElapsedMilliseconds
+            });
+        }
+    }
+}
---
+++ Demo/Demo/Models/Demo.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo.Models
+{
+    public class DemoSyncAsync
+    {
+        public static int A()
+        {
+            Thread.Sleep(2000);
+            return new Random().Next();
+        }
+        public static string B()
+        {
+            Thread.Sleep(5000);
+            return "HCMUE.NET";
+        }
+        public static void C()
+        {
+            Thread.Sleep(3000);
+        }
+    }
+}
