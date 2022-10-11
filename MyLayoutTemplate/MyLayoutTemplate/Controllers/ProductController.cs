2f950b64eae2963b0b118a5ebcd773ac6871f9f1
Hien Luong <hyhien@gmail.com>
2022-10-11 09:24:37 +0700

    Add partial view

--- MyLayoutTemplate/MyLayoutTemplate/Controllers/ProductController.cs
+++ MyLayoutTemplate/MyLayoutTemplate/Controllers/ProductController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyLayoutTemplate.Models;
 
 namespace MyLayoutTemplate.Controllers
 {
     public class ProductController : Controller
     {
+        public IActionResult GetCategories()
+        {
+            var data = new List<CategoryModel> {
+                new CategoryModel{Id = 1, Name = "IPhone 14"},
+                new CategoryModel{Id = 2, Name = "Mac Pro 2021"}
+            };
+            return PartialView("Category", data);
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
     }
 }
