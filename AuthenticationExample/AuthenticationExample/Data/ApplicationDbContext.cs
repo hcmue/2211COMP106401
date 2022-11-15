4e5419a29a531d14ab3f6f8e4a7f6e4d373fd014
Hien Luong <hyhien@gmail.com>
2022-11-15 08:32:22 +0700

    Demo authen auto

---
+++ AuthenticationExample/AuthenticationExample/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationExample.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Loai>? Loais { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ AuthenticationExample/AuthenticationExample/Data/Loai.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthenticationExample.Data
+{
+    public class Loai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
